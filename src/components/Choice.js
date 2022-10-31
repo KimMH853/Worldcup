5fcfc3119d238de85eb80484512d28af254551aa
KimMH853 <kimmh853@gmail.com>
2022-10-31 17:20:40 +0900

    이미지 배치

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -3,15 +3,17 @@ import items from "../lib/items";
 
 const Choice = () => {
   const [actors, setActors] = useState([]);
+
   useEffect(() => {
-    setActors(items[0], items[1]);
+    items.sort(() => Math.random() - 0.5);
+    setActors([items[0], items[1]]);
   }, []);
-
-  console.log(Array.isArray(items));
-
   return (
     <div>
       <h1>골라요</h1>
+      {actors.map((a) => {
+        return <img key={a.name} src={a.src} alt={a.name} />;
+      })}
     </div>
   );
 };
