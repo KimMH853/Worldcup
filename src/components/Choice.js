b75cf1fca11a56b7025236b0bbca75ad45b54948
KimMH853 <kimmh853@gmail.com>
2022-11-04 14:05:00 +0900

    branch test

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from "react";
 import items from "../lib/items";
 
 const Choice = () => {
+  const [arr, setArr] = useState([]);
   const [actors, setActors] = useState([]);
   const [winners, setWinners] = useState([]);
 
-  const clickHandler = (a) => {
-    console.log(a);
+  const clickHandler = (actor) => {
+    if (arr.length > 2) {
+      console.log(actor);
+      setWinners([...winners, actor]);
+      console.log(winners);
+      setActors([arr[2], arr[3]]);
+      setArr(arr.slice(2));
+    } else {
+      return null;
+    }
   };
 
   useEffect(() => {
     items.sort(() => Math.random() - 0.5);
+    setArr(items);
     setActors([items[0], items[1]]);
   }, []);
   return (
@@ -24,7 +34,7 @@ const Choice = () => {
               clickHandler(a);
             }}
           >
-            <img src={a.src} alt={a.name} />;
+            <img src={a.src} alt={a.name} />
           </div>
         );
       })}
